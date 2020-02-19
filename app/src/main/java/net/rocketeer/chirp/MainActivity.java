08fc1bdee837f49699cb2432f2c006dce91bb891
Ralph Tang <r33tang@uwaterloo.ca>
2020-02-19 15:21:04 -0500

    Init commit

---
+++ app/src/main/java/net/rocketeer/chirp/MainActivity.java
@@ -0,0 +1,15 @@
+package net.rocketeer.chirp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
